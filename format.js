b3ea9839c66c140331ade252f6eda041fb0a1d56
Frank Rowe <fsrowe@gmail.com>
2015-10-15 16:15:16 -0400

    download

--- format.js
+++ format.js
@@ -1,21 +1,49 @@
 var fs = require('fs')
+  , http = require('http')
 
-var marylee = JSON.parse(fs.readFileSync('./marylee.json', 'utf8'))[0]
-
-var geojson = {
-  "type": "Feature",
-  "geometry": {
-    "type": "MultiPoint",
-    "coordinates": []
-  },
-  "properties": {
-    "time": []
-  }
+var options = {
+  host: 'www.ocearch.org',
+  path: '/tracker/ajax/filter-sharks/?sharks%5B%5D=41&tracking-activity=+&gender=+&stage-of-life=+&location=0'
 }
 
-marylee.pings.reverse().forEach(function(ping, idx) {
-  geojson.geometry.coordinates.push([+ping.longitude, +ping.latitude])
-  geojson.properties.time.push(new Date(ping.datetime))
-})
+http.request(options, function(response) {
+  var str = '';
 
-fs.writeFileSync('./js/marylee-track.js', 'var track = ' + JSON.stringify(geojson))
+  response.on('data', function (chunk) {
+    str += chunk
+  })
+
+  response.on('end', function () {
+    format(JSON.parse(str))
+    save(JSON.parse(str))
+  })
+
+}).end()
+
+function format(json) {
+  var marylee = json[0]
+
+  var geojson = {
+    "type": "Feature",
+    "geometry": {
+      "type": "MultiPoint",
+      "coordinates": []
+    },
+    "properties": {
+      "time": []
+    }
+  }
+
+  marylee.pings.reverse().forEach(function(ping, idx) {
+    geojson.geometry.coordinates.push([+ping.longitude, +ping.latitude])
+    geojson.properties.time.push(new Date(ping.datetime))
+  })
+
+  fs.writeFileSync('./js/marylee-track.js', 'var track = ' + JSON.stringify(geojson))
+}
+
+
+function save(json) {
+  fs.writeFileSync('./marylee.json', JSON.stringify(json))
+}
+
